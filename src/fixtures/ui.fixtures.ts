8bca0a09b3113a0b2764b95f41f30604c2cbcab0
Tristan Santiago <37146990+esotericwarrior@users.noreply.github.com>
2026-01-01 09:08:23 -0500

    Added navbar component to ui.fixtures.ts.

--- src/fixtures/ui.fixtures.ts
+++ src/fixtures/ui.fixtures.ts
@@ -2,15 +2,18 @@ import { test as base } from '@playwright/test';
 import { HomePage } from '@/pages/home.page';
 import { ProductPage } from '@/pages/product.page';
 import { CartPage } from '@/pages/cart.page';
+import { NavbarComponent } from '@/components/navbar.component';
 
 export const test = base.extend<{
   home: HomePage;
   product: ProductPage;
   cart: CartPage;
+  navbar: NavbarComponent;
 }>({
   home: async ({ page }, use) => { await use(new HomePage(page)); },
   product: async ({ page }, use) => { await use(new ProductPage(page)); },
   cart: async ({ page }, use) => { await use(new CartPage(page)); },
+  navbar: async ({ page }, use) => { await use(new NavbarComponent(page)); },
 });
 
 // export const expect = test.expect;
